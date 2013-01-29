ccc837c3ddd766719f65f4dc3f554771fc42c842
Alistair MacDonald <Alistair.MacDonald@illumina.co.uk>
2013-01-29 17:24:38 +0000

    OPEN - issue YZABP-145: UMH - Current Conditions pages https://illuminadev.co.uk/jira/browse/YZABP-145
    Create the "edit"/update actions for the condition. Still need to save
    the data though.

--- Condition.php
+++ Condition.php
@@ -4,6 +4,9 @@ namespace Illumina\UnderstandingMyHealthBundle\Entity;
 use Symfony\Component\Validator\Constraints as Assert;
 use Illumina\PhphealthvaultBundle\Validator\Constraints as Validate;
 
+use com\microsoft\wc\thing\condition\Condition as HealthvaultCondition;
+use com\microsoft\wc\thing\Thing;
+
 class Condition
 {
     /**
@@ -46,6 +49,17 @@ class Condition
      */
     protected $notes;
     
+    /**
+     * @var \com\microsoft\wc\thing\thing\Thing
+     */
+    protected $hvCondition;
+    
+    public function __construct(Thing $hvCondition = NULL)
+    {
+        if ($hvCondition) {
+            $this->setHealthvaultCondition($hvCondition);
+        }
+    }
     
     public function getName()
     {
@@ -59,6 +73,10 @@ class Condition
     {
         $this->name = $name;
         
+        if ($this->hvCondition) {
+            $this->setHealthvaultName($name);
+        }
+        
         return $this;
     }
     
@@ -70,6 +88,10 @@ class Condition
     public function setCategory($category)
     {
         $this->category = $category;
+
+        if ($this->hvCondition) {
+            $this->setHealthvaultCategory($category);
+        }
         
         return $this;
     }
@@ -83,6 +105,10 @@ class Condition
     {
         $this->status = $status;
         
+        if ($this->hvCondition) {
+            $this->setHealthvaultStatus($status);
+        }
+        
         return $this;
     }
     
@@ -94,6 +120,10 @@ class Condition
     public function setOnsetDate($onsetDate)
     {
         $this->onsetDate = $onsetDate;
+        
+        if ($this->hvCondition) {
+            $this->setHealthvaultOnsetDate($onsetDate);
+        }
         
         return $this;
     }
@@ -107,6 +137,10 @@ class Condition
     {
         $this->stopDate = $stopDate;
         
+        if ($this->hvCondition) {
+            $this->setHealthvaultStopDate($stopDate);
+        }
+        
         return $this;
     }
     
@@ -118,6 +152,10 @@ class Condition
     public function setStopReason($stopReason)
     {
         $this->stopReason = $stopReason;
+        
+        if ($this->hvCondition) {
+            $this->setHealthvaultStopReason($stopReason);
+        }
         
         return $this;
     }
@@ -131,11 +169,14 @@ class Condition
     {
         $this->notes = $notes;
         
+        if ($this->hvCondition) {
+            $this->setHealthvaultNotes($notes);
+        }
+        
         return $this;
     }
-    
-    
-    static public function getPossibleCategories()
+       
+    public static function getPossibleCategories()
     {
         return array(
             'Lorem ipsum dolor sit amet.',
@@ -147,5 +188,162 @@ class Condition
             'Duis pharetra enim ut.',
             'Integer ac lacus risus.',
         );
+    }
+    
+    public static function supports(Thing $thing)
+    {
+        return ($thing->getTypeId()->getValue() == HealthvaultCondition::ID);
+    }
+        
+    public function getVersion() {
+        if ( ! $this->hvCondition )
+        {
+            return FALSE;
+        }
+        
+        return $this->hvCondition->getThingId()->getVersionStamp();
+    }
+    
+    public function getId()
+    {
+        if ( ! $this->hvCondition )
+        {
+            return FALSE;
+        }
+        
+        return $this->hvCondition->getThingId()->getValue();
+    }
+    
+    
+    public function setHealthvaultCondition(Thing $hvCondition)
+    {
+        if ( ! self::supports($hvCondition) )
+        {
+            return FALSE;
+        }
+        
+        $this->hvCondition = $hvCondition;
+        
+        $payload = $this->getHealthvaultPayload();
+        
+        $this->name = $payload->getName()->getText();
+        $this->onsetDate = $this->getHealthvaultDate($payload->getOnsetDate());
+        $this->stopDate = $this->getHealthvaultDate($payload->getStopDate());
+        $this->stopReason = $payload->getStopReason();
+        
+        $codes = $payload->getStatus()->getCode();
+        $code = array_shift($codes);
+        
+        $this->status = $code->getValue();
+        $dataXml = $this->hvCondition->getDataXml();
+        $payloadArea = array_shift($dataXml);
+        $this->notes = $payloadArea->getCommon()->getNote();
+        // $this->category = ?
+        
+        return TRUE;
+    }
+    
+    public function getHealthvaultCondition(HealthvaultCondition $hvCondition = NULL)
+    {
+        if ( ! $hvCondition && ! $this->hvCondition )
+        {
+            $hvCondition = new HealthvaultCondition();
+            $this->hvCondition = $hvCondition;
+        }
+        
+        $this->setHealthvaultCategory($this->category);
+        $this->setHealthvaultStatus($this->status);
+        $this->setHealthvaultOnsetDate($this->onsetDate);
+        $this->setHealthvaultStopDate($this->stopDate);
+        $this->setHealthvaultStopReason($this->stopReason);
+        $this->setHealthvaultNotes($this->notes);
+        
+        return $hvCondition;
+    }
+    
+    protected function getHealthvaultPayload()
+    {
+        $data = $this->hvCondition->getDataXml();
+        $payloadWrapper = array_shift($data);
+        $payloadContentArr = $payloadWrapper->getAny();
+        $payloadContent = array_shift($payloadContentArr);
+        
+        return $payloadContent;
+    }
+    
+    protected function setHealthvaultName($name)
+    {
+        $hvName = $this->getHealthvaultPayload()->getName();
+        $hvName->setText($name);
+    }
+    
+    protected function setHealthvaultCategory($category)
+    {
+        // FIXME: Where does this map?
+    }
+    
+    protected function setHealthvaultOnsetDate($onsetDate)
+    {
+        $hvOnsetDate = $this->getHealthvaultPayload()->getOnsetDate();
+        
+        $this->setHealthvaultDate($hvOnsetDate, $onsetDate);
+    }
+    
+    protected function setHealthvaultStopDate($stopDate)
+    {
+        $hvStopDate = $this->getHealthvaultPayload()->getStopDate();
+        
+        $this->setHealthvaultDate($hvStopDate, $stopDate);
+    }
+    
+    protected function setHealthvaultStopReason($stopReason)
+    {
+        $this->getHealthvaultPayload()->setStopReason($stopReason);
+    }   
+    
+    protected function setHealthvaultStatus($status)
+    {
+        $hvStatus = $this->getHealthvaultPayload()->getStatus();
+        $hvStatus->setCode($status);
+    }
+
+    protected function setHealthvaultNotes($notes)
+    {
+        $hvCommon = $this->hvCondition->getDataXml()->getCommon();
+        $hvCommon->setNote($notes);
+    }
+    
+    protected function setHealthvaultDate(\com\microsoft\wc\dates\ApproxDateTime $hvDate, $date)
+    {
+        if ( ! $date instanceof \DateTime) {
+            try {
+                $dateObj = new \DateTime($date);
+                
+                $structuredDate = $hvDate->getStructured()->getDate();
+                $structuredDate->getY()->setValue($dateObj->format('Y'));
+                $structuredDate->setM()->setValue($dateObj->format('m'));
+                $structuredDate->setD()->setValue($dateObj->format('d'));
+            }
+            catch (\Exception $e) {
+                error_log('Failed to convert date to DateTime: ' . print_r($date, TRUE));
+            }
+        }
+    }
+    
+    protected function getHealthvaultDate(\com\microsoft\wc\dates\ApproxDateTime $hvDate)
+    {
+        $structuredDate = $hvDate->getStructured()->getDate();
+        
+        $y = $structuredDate->getY()->getValue();
+        $m = $structuredDate->getM()->getValue();
+        $d = $structuredDate->getD()->getValue();
+        
+        if ( ! empty($y) && ! empty($m) && ! empty($d)) {
+            $dateObj = new \DateTime();
+            $dateObj->setDate($y, $m, $d);
+            return $dateObj;
+        }
+        
+        return NULL;
     }
 }
