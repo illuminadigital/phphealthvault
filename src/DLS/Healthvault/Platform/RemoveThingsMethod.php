1c655e0d18014da23d6238e16fabfad1f22164f9
Alistair MacDonald <Alistair.MacDonald@illumina.co.uk>
2013-02-20 13:17:04 +0000

    Add in the "RemoveThings" method

---
+++ src/DLS/Healthvault/Platform/RemoveThingsMethod.php
@@ -0,0 +1,23 @@
+<?php
+namespace DLS\Healthvault\Platform;
+
+use com\microsoft\wc\thing\ThingKey;
+
+class RemoveThingsMethod extends PlatformMethod
+{
+    protected $methodName = 'RemoveThings';
+    
+    // This reads better
+    public function includeThing($thingId, $versionId) {
+        $this->addThing($thingId, $versionId);
+    }
+    
+    public function addThing($thingId, $versionId)
+    {
+        $thingKey = new ThingKey();
+        $thingKey->setValue($thingId);
+        $thingKey->setVersionStamp($versionId);
+        
+        $this->requestData->getInfo()->addThingId($thingKey);
+    }
+}
