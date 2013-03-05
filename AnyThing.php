75421a6ce01e1ab52489cb612106912b296a2e89
Alistair MacDonald <Alistair.MacDonald@illumina.co.uk>
2013-03-05 17:37:20 +0000

    OPEN - issue YZABP-167: Add "Are you sure?" prompt to Delete operations https://illuminadev.co.uk/jira/browse/YZABP-167
    Implement delete confirmation in the same way to personal

--- AnyThing.php
+++ AnyThing.php
@@ -15,4 +15,31 @@ class AnyThing extends BaseThing
     protected function getNewDataXmlContent() {
         return NULL;
     }
+    
+    public static function getProxy($thingId, $version = NULL) {
+        $thing = new Thing();
+        
+        $thingKey = $thing->getThingId();
+        $thingKey->setValue($thingId);
+        
+        if ( ! empty($version) )
+        {
+            $thingKey->setVersionStamp($version);
+        }
+        
+        $thing->setThingId($thingKey);
+         
+        return new AnyThing($thing);
+    }
+    
+    public function setId($id) {
+        $this->id = $id;
+        
+        if ( $this->thing )
+        {
+            $this->setThingId($id);
+        }
+    
+        return $this;
+    }
 }
