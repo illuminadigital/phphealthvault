d4e6d95ef12c17f9faf8ac00c5d5bd8da18a9876
Alistair MacDonald <Alistair.MacDonald@illumina.co.uk>
2013-03-08 16:56:41 +0000

    OPEN - issue YZABP-179: HT - Documents and Letters https://illuminadev.co.uk/jira/browse/YZABP-179
    Add basic index support for documents

---
+++ File.php
@@ -0,0 +1,91 @@
+<?php
+namespace Illumina\HealthTrackingBundle\Entity;
+
+use Illumina\PhphealthvaultBundle\Entity\BaseThing;
+
+use com\microsoft\wc\thing\Thing2;
+use com\microsoft\wc\thing\file\File as hvFile;
+
+class File extends BaseThing
+{
+    protected $size;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    public function setThing(Thing2 $thing)
+    {
+        $result = parent::setThing($thing);
+        
+        if ( ! $result )
+        {
+            return $result;
+        }
+        
+        $payload = $this->getThingPayload();
+        
+        $this->name = $payload->getName()->getValue();
+        
+        return $this;
+    }
+
+    public function getThing(Thing2 $thing = NULL)
+    {
+        $thing = parent::getThing($thing);
+        
+        $this->setThingName($this->name);
+        
+        return $thing;
+    }
+
+    public function getSize()
+    {
+        if ($this->thing) {
+            return $this->getThingSize();
+        }
+        // else
+        return NULL;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        
+        if ($this->thing) {
+            $this->setThingName($name);
+        }
+        
+        return $this;
+    }
+    
+    public function setThingName($name)
+    {
+        $payload = $this->getThingPayload();
+        $payload->getName()->setValue($name);
+        
+        return $this;
+    }
+    
+    public static function reallySupports(Thing2 $thing)
+    {
+        return ($thing->getTypeId()->getValue() == hvFile::ID);
+    }
+    
+    public function getNewDataXmlContent() {
+        return new hvFile();
+    }
+    
+    public function getThingSize()
+    {
+        $payload = $this->getThingPayload();
+        
+        return $payload->getSize();
+    }
+}
