7c7a96ab399ff8b888be81786a29ffff0dc90e09
Alistair MacDonald <Alistair.MacDonald@illumina.co.uk>
2013-03-15 14:29:59 +0000

    OPEN - issue YZABP-181: HT - Fitness https://illuminadev.co.uk/jira/browse/YZABP-181
    Make the interface a bit more fluid
    Add in the missing get/set methods for the unitsCode

--- src/DLS/Healthvault/Proxy/Type/DisplayValue.php
+++ src/DLS/Healthvault/Proxy/Type/DisplayValue.php
@@ -1,197 +1,234 @@
 <?php
 namespace DLS\Healthvault\Proxy\Type;
+abstract class DisplayValue implements DisplayValueInterface
+{
 
-abstract class DisplayValue implements DisplayValueInterface {
+    /**
+     * @var double
+     */
+    protected $normalisedValue;
 
-	/**
-	 * @var double
-	 */
-	protected $normalisedValue;
-	
-	/**
-	 * 
-	 * @var double
-	 */
-	protected $majorValue;
+    /**
+     * 
+     * @var double
+     */
+    protected $majorValue;
 
-	/**
-	 * 
-	 * @var double
-	 */
-	protected $minorValue;
+    /**
+     * 
+     * @var double
+     */
+    protected $minorValue;
 
-	/**
-	 * 
-	 * @var string
-	 */
-	protected $unitsCode;
-	
-	public static function getTypeOptionChoices() {
-		return array_keys(self::getTypeOptions());
-	}
+    /**
+     * 
+     * @var string
+     */
+    protected $unitsCode;
 
-	
-	public function getMajorValue() {
-		return $this->majorValue;
-	}
+    public static function getTypeOptionChoices()
+    {
+        return array_keys(self::getTypeOptions());
+    }
 
-	public function setMajorValue($majorValue) {
-		$this->majorValue = $majorValue;
-	}
+    public function getMajorValue()
+    {
+        return $this->majorValue;
+    }
 
-	public function getMinorValue() {
-		return $this->minorValue;
-	}
+    public function setMajorValue($majorValue)
+    {
+        $this->majorValue = $majorValue;
+        
+        return $this;
+    }
 
-	public function setMinorValue($minorValue) {
-		$this->minorValue = $minorValue;
-	}
+    public function getMinorValue()
+    {
+        return $this->minorValue;
+    }
 
-	public function getNormalisedValue() {
-		return $this->normalisedValue;
-	}
+    public function setMinorValue($minorValue)
+    {
+        $this->minorValue = $minorValue;
+        
+        return $this;
+    }
 
-	public function setNormalisedValue($normalisedValue) {
-		$this->normalisedValue = $normalisedValue;
-	}
+    public function getUnitsCode()
+    {
+        return $this->unitsCode;
+    }
 
-	public function getSelectedTypeData($unitsCode = NULL) {
-		$typeOptions = self::getTypeOptions();
-		
-		if ( empty($unitsCode) ) {
-		    $unitsCode = $this->unitsCode;
-		}
-		
-		if (isset($typeOptions[$unitsCode])) {
-			return $typeOptions[$unitsCode];
-		}
-		// else
-		return NULL;
-	}
-	
-	public function getUnits() {
-		$thisType = $this->getSelectedTypeData();
-		
-		if ( ! $thisType) {
-			// Assume the major value is normalised and complete
-			return $this->getMajorValue();
-		} 
-		// else
-		$units = ($this->minorValue * $thisType['minor_scale']) + $this->majorValue;
-		if (isset($thisType['major_scale']) && $thisType['major_scale'] != 1) {
-			$units = $units * $thisType['major_scale'];
-		}
-		return $units;
-	}
-	
-	public function getDisplayValue() {
-		$thisType = $this->getSelectedTypeData();
-		
-		if ( ! $thisType) {
-			return $this->majorValue;
-		} else {
-			return self::getValueString($this->majorValue, $this->minorValue, $thisType);
-		}
-	}
-	
-	public static function getValueString($majorValue, $minorValue, $typeData) {
-		$majorName = $typeData['major'];
-		$minorName = $typeData['minor'];
-		
-		$text = sprintf('%d %s', $majorValue, $majorName);
-		if ($minorValue != 0) {
-			$text .= sprintf(' %d %s', $minorValue, $minorName);
-		}
-		
-		return $text;
-	}
-	
-	public function setFromNormalisedUnits($units) {
-		$thisType = $this->getSelectedTypeData();
-		
-		if ( ! $thisType) {
-			// Assume the units value is normalised and complete
-			$this->majorValue = $units;
-			$this->minorValue = 0;
-		} else {
-			$convertedUnits = (isset($thisType['major_scale']) ? $units / $thisType['major_scale'] : $units);
-			$this->majorValue = (int) $convertedUnits;
-			$this->minorValue = ($convertedUnits - $this->majorValue) / $thisType['minor_scale'];
-		}
-	}
-	
-	public function setFromUnits($units) {
-		$thisType = $this->getSelectedTypeData();
-		
-		if ( ! $thisType) {
-			// Assume the units value is normalised and complete
-			$this->majorValue = $units;
-			$this->minorValue = 0;
-		} else {
-			$this->majorValue = (int) $units;
-			$this->minorValue = ($units - $this->majorValue) / $thisType['minor_scale'];
-		}
-	}
-	
-	public function setFromThingElement($thingElement) {
-		$display = $thingElement->getDisplay();
-		
-		if (empty($display)) {
-			return FALSE;
-		}
-		
-		$unitsCode = $display->getUnitsCode();
-		$units = $display->getUnits();
-		
-		if ( ! empty($unitsCode) && $this>getSelectedTypeData($unitsCode) )
-		{
-			$this->unitsCode = $unitsCode;
-		} else if ( ! empty($units) && ($unitsCode = $this->getCodeForTypeName($units)) ) {
-			$this->unitsCode = $unitsCode;
-		} else {
-			$this->unitsCode = NULL;
-		}
+    public function setUnitsCode($unitsCode)
+    {
+        $this->unitsCode = $unitsCode;
+        
+        return $this;
+    }
 
-		$units = $display->getUnits();
-		
-		if ( isset($units) ) {
-			$this->setFromUnits($value);
-		} else {
-			$this->setFromUnits($display->getValue());
-		}
-		
-		// We can't set the normalised value. The outer code needs to
-		
-		return $thingElement;
-	}
-	
-	public function updateToThingElement($thingElement) {
-		$display = $thingElement->getDisplay();
-		
-		if ( empty($display) ) {
-			return FALSE;
-		}
-	}
-	
-	public function getSelectedUnitData($unitCode = NULL) {
-		$data = self::getTypeOptions();
-		
-		if (isset($data[$unitCode])) {
-			return $data[$unitCode];
-		}
-		// else
-		return NULL;
-	}
-	
-	public function getCodeForTypeName($name) {
-		$data = self::getTypeOptions();
+    public function getNormalisedValue()
+    {
+        return $this->normalisedValue;
+    }
 
-		foreach ($data as $code => $datum) {
-			if (isset($datum['name']) && $datum['name'] == $name) {
-				return $code;
-			}
-		}
-		
-		return NULL;
-	}
+    public function setNormalisedValue($normalisedValue)
+    {
+        $this->normalisedValue = $normalisedValue;
+    }
+
+    public function getSelectedTypeData($unitsCode = NULL)
+    {
+        $typeOptions = self::getTypeOptions();
+
+        if (empty($unitsCode)) {
+            $unitsCode = $this->unitsCode;
+        }
+
+        if (isset($typeOptions[$unitsCode])) {
+            return $typeOptions[$unitsCode];
+        }
+        // else
+        return NULL;
+    }
+
+    public function getUnits()
+    {
+        $thisType = $this->getSelectedTypeData();
+
+        if (!$thisType) {
+            // Assume the major value is normalised and complete
+            return $this->getMajorValue();
+        }
+        // else
+        $units = ($this->minorValue * $thisType['minor_scale'])
+                + $this->majorValue;
+        if (isset($thisType['major_scale']) && $thisType['major_scale'] != 1) {
+            $units = $units * $thisType['major_scale'];
+        }
+        return $units;
+    }
+
+    public function getDisplayValue()
+    {
+        $thisType = $this->getSelectedTypeData();
+
+        if (!$thisType) {
+            return $this->majorValue;
+        } else {
+            return self::getValueString($this->majorValue, $this->minorValue,
+                    $thisType);
+        }
+    }
+
+    public static function getValueString($majorValue, $minorValue, $typeData)
+    {
+        $majorName = $typeData['major'];
+        $minorName = $typeData['minor'];
+
+        $text = sprintf('%d %s', $majorValue, $majorName);
+        if ($minorValue != 0) {
+            $text .= sprintf(' %d %s', $minorValue, $minorName);
+        }
+
+        return $text;
+    }
+
+    public function setFromNormalisedUnits($units)
+    {
+        $thisType = $this->getSelectedTypeData();
+
+        if (!$thisType) {
+            // Assume the units value is normalised and complete
+            $this->majorValue = $units;
+            $this->minorValue = 0;
+        } else {
+            $convertedUnits = (isset($thisType['major_scale']) ? $units
+                            / $thisType['major_scale'] : $units);
+            $this->majorValue = (int) $convertedUnits;
+            $this->minorValue = ($convertedUnits - $this->majorValue)
+                    / $thisType['minor_scale'];
+        }
+    }
+
+    public function setFromUnits($units)
+    {
+        $thisType = $this->getSelectedTypeData();
+
+        if (!$thisType) {
+            // Assume the units value is normalised and complete
+            $this->majorValue = $units;
+            $this->minorValue = 0;
+        } else {
+            $this->majorValue = (int) $units;
+            $this->minorValue = ($units - $this->majorValue)
+                    / $thisType['minor_scale'];
+        }
+    }
+
+    public function setFromThingElement($thingElement)
+    {
+        $display = $thingElement->getDisplay();
+
+        if (empty($display)) {
+            return FALSE;
+        }
+
+        $unitsCode = $display->getUnitsCode();
+        $units = $display->getUnits();
+
+        if (!empty($unitsCode) && $this > getSelectedTypeData($unitsCode)) {
+            $this->unitsCode = $unitsCode;
+        } else if (!empty($units)
+                && ($unitsCode = $this->getCodeForTypeName($units))) {
+            $this->unitsCode = $unitsCode;
+        } else {
+            $this->unitsCode = NULL;
+        }
+
+        $units = $display->getUnits();
+
+        if (isset($units)) {
+            $this->setFromUnits($value);
+        } else {
+            $this->setFromUnits($display->getValue());
+        }
+
+        // We can't set the normalised value. The outer code needs to
+
+        return $thingElement;
+    }
+
+    public function updateToThingElement($thingElement)
+    {
+        $display = $thingElement->getDisplay();
+
+        if (empty($display)) {
+            return FALSE;
+        }
+    }
+
+    public function getSelectedUnitData($unitCode = NULL)
+    {
+        $data = self::getTypeOptions();
+
+        if (isset($data[$unitCode])) {
+            return $data[$unitCode];
+        }
+        // else
+        return NULL;
+    }
+
+    public function getCodeForTypeName($name)
+    {
+        $data = self::getTypeOptions();
+
+        foreach ($data as $code => $datum) {
+            if (isset($datum['name']) && $datum['name'] == $name) {
+                return $code;
+            }
+        }
+
+        return NULL;
+    }
 }
