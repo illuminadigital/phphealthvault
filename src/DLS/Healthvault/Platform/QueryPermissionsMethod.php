f9669c76ca0b1bd7736a905c83a91c020ab4208c
Josep Bonet <boncelor@gmail.com>
2013-04-26 14:45:48 +0100

    Added QueryPermissionsMethod.

---
+++ src/DLS/Healthvault/Platform/QueryPermissionsMethod.php
@@ -0,0 +1,27 @@
+<?php
+namespace DLS\Healthvault\Platform;
+
+use DLS\Healthvault\HealthvaultConfigurationInterface;
+
+use com\microsoft\wc\types\Guid;
+
+class QueryPermissionsMethod extends PlatformMethod
+{
+    protected $methodName = 'QueryPermissions';
+    protected $methodVersion = 1;
+
+    public function __construct(HealthvaultConfigurationInterface $configuration)
+    {
+        $this->mustIncludeRecord = TRUE;
+        parent::__construct($configuration);
+    }
+
+    public function addThingTypeId($thingId){
+
+        $thingGuid = new Guid($thingId);
+
+        $queryInfo = $this->requestData->getInfo()->addThingTypeId($thingGuid);
+
+    }
+
+}
