56abe45f95fad711e8068fe4cdd3a0465fb37557
Alistair MacDonald <Alistair.MacDonald@illumina.co.uk>
2013-06-06 11:14:08 +0100

    API Break:
    Make the Weight proxy use the DisplayValue type

--- src/DLS/Healthvault/Proxy/Thing/Weight.php
+++ src/DLS/Healthvault/Proxy/Thing/Weight.php
@@ -3,130 +3,46 @@ namespace DLS\Healthvault\Proxy\Thing;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
+use DLS\Healthvault\Utilities\VocabularyInterface;
+
 use com\microsoft\wc\thing\Thing2;
 use com\microsoft\wc\thing\weight\Weight as hvWeight;
+
+use DLS\Healthvault\Proxy\Type\HeavyWeightValue;
 
 class Weight extends WhenThing
 {
     protected $thingType = 'Weight';
     
     /**
-     * @Assert\NotBlank()
-     * @Assert\Type(type="integer")
-     * @Assert\Min(0)
+     * @Assert\NotBlank
      * 
-     * @var integer
+     * @var \DLS\Healthvault\Proxy\Type\HeavyWeightValue
      */
-    protected $majorWeight;
+    protected $weight;
 
-    /**
-     * @Assert\NotBlank()
-     * @Assert\Type(type="integer")
-     * @Assert\Min(0)
-     * 
-     * @var integer
-     */
-    protected $minorWeight;
-
-    /**
-     * @Assert\NotBlank()
-     * @Assert\Choice(callback="getWeightOptionChoices")
-     * 
-     * @var string
-     */
-    protected $weightType;
-
-    protected static $weightTypes;
-
-    protected static function getWeightTypes()
+    public function __construct(Thing2 $thing = NULL,
+            VocabularyInterface $healthvaultVocabulary = NULL)
     {
-        if (empty(self::$weightTypes)) {
-            self::$weightTypes = array(
-                'stlb' => array(
-                    'name' => 'Stones',
-                    'major' => 'stone(s)',
-                    'minor' => 'pound(s)', 
-                    'minor_scale' => 1 / 14,
-                    'major_scale' => 6.35029
-                ),
-                'kg' => array(
-                    'name' => 'Kilograms',
-                    'major' => 'Kilogram(s)',
-                    'minor' => 'gram(s)',
-                    'minor_scale' => 0.001,
-                ),
-            );
-        }
-        
-        return self::$weightTypes;
-    }
-
-    public static function getWeightOptions()
-    {
-        static $options;
-
-        if (empty($options)) {
-            $options = array();
-
-            foreach (self::getWeightTypes() as $type => $data) {
-                $options[$type] = $data['name'];
-            }
-        }
-
-        return $options;
-    }
+        $this->weight = new HeavyWeightValue();
     
-    public static function getWeightOptionChoices() 
-    {
-        return array_keys(self::getWeightOptions());
+        parent::__construct($thing, $healthvaultVocabulary);
     }
     
     /**
-     * @return the integer
+     * @return the weight
      */
-    public function getMajorWeight()
+    public function getWeight()
     {
-        return $this->majorWeight;
+        return $this->weight;
     }
 
     /**
-     * @param  $majorWeight
+     * @param  $weight
      */
-    public function setMajorWeight($majorWeight)
+    public function setWeight($weight)
     {
-        $this->majorWeight = $majorWeight;
-    }
-
-    /**
-     * @return the integer
-     */
-    public function getMinorWeight()
-    {
-        return $this->minorWeight;
-    }
-
-    /**
-     * @param  $minorWeight
-     */
-    public function setMinorWeight($minorWeight)
-    {
-        $this->minorWeight = $minorWeight;
-    }
-
-    /**
-     * @return the string
-     */
-    public function getWeightType()
-    {
-        return $this->weightType;
-    }
-
-    /**
-     * @param  $weightType
-     */
-    public function setWeightType($weightType)
-    {
-        $this->weightType = $weightType;
+        $this->weight = $weight;
     }
 
     protected function getNewDataXmlContent()
@@ -136,7 +52,7 @@ class Weight extends WhenThing
     
     public static function reallySupports(Thing2 $thing)
     {
-        return ($thing->getTypeId()->getValue() == hvWeight::ID);
+        return (strtolower($thing->getTypeId()->getValue()) == strtolower(hvWeight::ID));
     }
     
     public function setThing(Thing2 $thing)
@@ -151,41 +67,8 @@ class Weight extends WhenThing
         $payload = $this->getThingPayload();
         
         $value = $payload->getValue();
-        $display = $value->getDisplay();
         
-        $weightTypes = $this->getWeightTypes();
-        
-        $units = (double) $display->getValue();
-            
-        if (isset($weightTypes[$display->getUnitsCode()]))
-        {
-            $thisType = $weightTypes[$display->getUnitsCode()];
-            
-            if ($units > 0) {
-                $majorUnits = (int) $units;
-                $minorUnits = ($units - $majorUnits) / $thisType['minor_scale'];
-                                 
-            } else {
-                $minor_scale = $thisType['minor_scale'];
-                $major_scale = (isset($thisType['major_scale']) ? $thisType['major_scale'] : 1);
-                
-                $units = $value / $major_scale;
-                
-                $minorThreshold = 1 / $minor_scale;
-                
-                $majorUnits = (int) $units;
-                $minorUnits = round(($units - $majorUnits) / $minor_scale);
-            }
-        }
-        else
-        {
-            $majorUnits = $units;
-            $minorUnits = 0;
-        }
-        
-        $this->majorWeight = $majorUnits;
-        $this->minorWeight = $minorUnits;
-        $this->weightType = $display->getUnitsCode();
+        $this->weight->setFromThingElement($value);
         
         return $result;
     }
@@ -194,65 +77,16 @@ class Weight extends WhenThing
     {
         $thing = parent::getThing($thing);
         
-        $this->setThingValue((int)$this->majorWeight, (int)$this->minorWeight, $this->weightType);
+        $payload = $this->getThingPayload();
+        
+        if ( ! is_object($this->weight) || $this->weight->isEmpty() ) {
+            $thing->setValue(NULL);
+            
+        } else {
+            $this->weight->updateToThingElement($payload->getValue());
+        }
         
         return $thing;
-    }
-    
-    protected function setThingValue($majorWeight, $minorWeight, $weightType)
-    {
-        $weightTypes = $this->getWeightTypes();
-        
-        if ( ! isset($weightTypes[$weightType])) {
-            return FALSE;
-        }
-        
-        $typeData = $weightTypes[$weightType];
-        
-        $text = $this->getValueString($majorWeight, $minorWeight, $weightType);
-        
-        $value = ($minorWeight * $typeData['minor_scale']) + $majorWeight;
-        
-        $needsNormalization = (isset($typeData['major_scale']) && $typeData['major_scale'] != 1);
-        
-        $normalizedValue = ($needsNormalization) ? $value * $typeData['major_scale'] : $value;
-
-        $payload = $this->getThingPayload();
-        $hvValue = $payload->getValue();
-        $hvValue->getKg()->setValue($normalizedValue);
-        $display = $hvValue->getDisplay();
-        
-        $display->setValue($value);
-        $display->setUnits($typeData['name']);
-        $display->setUnitsCode($weightType);
-        $display->setText($text);
-        
-        return $this;
-    }
-    
-    protected function getValueString($majorWeight, $minorWeight, $weightType)
-    {
-        $weightTypes = $this->getWeightTypes();
-        
-        if ( ! isset($weightTypes[$weightType])) {
-            return FALSE;
-        }
-        
-        $typeData = $weightTypes[$weightType];
-        
-        $majorName = $typeData['major'];
-        $minorName = $typeData['minor'];
-        
-        $text = sprintf('%d %s', $majorWeight, $majorName);
-        if ($minorWeight != 0) {
-            $text .= sprintf(' %d %s', $minorWeight, $minorName);
-        }
-        
-        return $text;
-    }
-    
-    public function getWeight() {
-        return $this->getValueString($this->majorWeight, $this->minorWeight, $this->weightType);
     }
     
     public static function getDisplayFields()
